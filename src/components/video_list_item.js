41763f28413cc55d911942e35689e85d645e17ca
marcinkowal2015 <marcinkowal2015@outlook.com>
2016-11-01 18:27:40 +0100

    Finished project

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,19 @@
-/**
- * Created by marcink on 16.10.2016.
- */
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <image className="media-object" src={imageUrl}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
